4b0b1cf4cfc0a3606d63a90ebbec75c3a58c1c3b
huanglongyun <974859862@qq.com>
2024-01-13 21:30:56 +0800

    v2 vdom渲染

--- main.js
+++ main.js
@@ -2,13 +2,41 @@
 // document.querySelector("#root").innerHTML="<h1>hello world!</h1>";
 
 // v1
-const h1dom=document.createElement("h1");
-h1dom.id="title";
-const rootdom=document.querySelector("#root");
+// const h1dom=document.createElement("h1");
+// h1dom.id="title";
+// const rootdom=document.querySelector("#root");
+// rootdom.appendChild(h1dom);
+
+// const textNode=document.createTextNode("")
+// textNode.textContent="hello world!";
+// h1dom.appendChild(textNode);
+
+// v2
+// react ：vdom渲染，一个js对象 type props children
+const textEl = {
+  type: "TEXT_ELEMENT",
+  props: {
+    nodeValue: "hello world!",
+    children: [],
+  },
+};
+const el = {
+  type: "h1",
+  props: {
+    id: "title",
+    children: [textEl],
+  },
+};
+
+const h1dom = document.createElement(el.type);
+h1dom.id = el.props.id;
+const rootdom = document.querySelector("#root");
 rootdom.appendChild(h1dom);
 
-const textNode=document.createTextNode("")
-textNode.textContent="hello world!";
+const textNode = document.createTextNode("");
+textNode.textContent = textEl.props.nodeValue;
 h1dom.appendChild(textNode);
+
+
 
 
