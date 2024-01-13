06391f26dfdf97b64b74ae651f9f5e278df10fd6
huanglongyun <974859862@qq.com>
2024-01-13 21:34:25 +0800

    v3 动态创建vdom

--- main.js
+++ main.js
@@ -13,30 +13,60 @@
 
 // v2
 // react ：vdom渲染，一个js对象 type props children
-const textEl = {
-  type: "TEXT_ELEMENT",
-  props: {
-    nodeValue: "hello world!",
-    children: [],
-  },
-};
-const el = {
-  type: "h1",
-  props: {
-    id: "title",
-    children: [textEl],
-  },
+// const textEl = {
+//   type: "TEXT_ELEMENT",
+//   props: {
+//     nodeValue: "hello world!",
+//     children: [],
+//   },
+// };
+// const el = {
+//   type: "h1",
+//   props: {
+//     id: "title",
+//     children: [textEl],
+//   },
+// };
+
+// const h1dom = document.createElement(el.type);
+// h1dom.id = el.props.id;
+// const rootdom = document.querySelector("#root");
+// rootdom.appendChild(h1dom);
+
+// const textNode = document.createTextNode("");
+// textNode.textContent = textEl.props.nodeValue;
+// h1dom.appendChild(textNode);
+
+// v3 动态创建vdom
+const createTextNode = (text) => {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
 };
 
-const h1dom = document.createElement(el.type);
-h1dom.id = el.props.id;
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+};
+
+const textEl = createTextNode("hello world!");
+const App = createElement("h1", { id: "title" }, textEl);
+console.log("app", App);
+
+const h1dom = document.createElement(App.type);
+h1dom.id = App.props.id;
 const rootdom = document.querySelector("#root");
 rootdom.appendChild(h1dom);
 
 const textNode = document.createTextNode("");
 textNode.textContent = textEl.props.nodeValue;
 h1dom.appendChild(textNode);
-
-
-
-
