180b331a0d07beb17124ddf7ef09bbd99eb86a1d
huanglongyun <974859862@qq.com>
2024-01-13 21:29:56 +0800

    v1 稍微具体一点

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // v0
-document.querySelector("#root").innerHTML="<h1>hello world!</h1>";
+// document.querySelector("#root").innerHTML="<h1>hello world!</h1>";
+
+// v1
+const h1dom=document.createElement("h1");
+h1dom.id="title";
+const rootdom=document.querySelector("#root");
+rootdom.appendChild(h1dom);
+
+const textNode=document.createTextNode("")
+textNode.textContent="hello world!";
+h1dom.appendChild(textNode);
+
 
